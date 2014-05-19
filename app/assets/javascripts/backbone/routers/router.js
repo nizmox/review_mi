8db7ceb14b56a47cc5e2ccff652abc0979ce1722
Nizmox <nizmox@gmail.com>
2014-05-19 12:00:55 +1000

    various fixes

--- app/assets/javascripts/backbone/routers/router.js
+++ app/assets/javascripts/backbone/routers/router.js
@@ -32,11 +32,11 @@ ReviewMi.Routers.appRouter = Backbone.Router.extend({
     this.movieSearch();
 
     //create a movies collection and search for specified title
-    var movies = new ReviewMi.Collections.Movies();
+    var movieResults = new ReviewMi.Collections.MovieResults();
 
     //perform search on OMDB for the specified search term
-    movies.searchOMDB(term, function () {
-      var view = new ReviewMi.Views.movieResultsListView({collection: movies});
+    movieResults.searchOMDB(term, function () {
+      var view = new ReviewMi.Views.movieResultsListView({collection: movieResults});
       view.render();
     });
     
@@ -45,44 +45,27 @@ ReviewMi.Routers.appRouter = Backbone.Router.extend({
   // view a single movie search result with more detail
   movieTitle: function (imdbID) {
 
-    var movieArr = ReviewMi.movies.where({ imdb_id: imdbID });
+    var movie = ReviewMi.movies.where({ imdb_id: imdbID })[0];
+
+    var render = function (movie) {
+      var view = new ReviewMi.Views.movieTitleView({model: movie});
+      view.render(); 
+    };
     
     //if movie does not already exist
-    if ( _.isEmpty( movieArr ) ) {
-      console.log('this is a new movie, searching omdb...');
-
-      var request = $.ajax({
-        type: 'GET',
-        dataType: 'json',
-        url: '/movies/search/' + imdbID
-        // data: { s: searchTerm }
-      //what to do on success
-      }).done(function (data) {
-
-        // create a new 'content' and 'movie' and add these to the respective collections
-        var content = new ReviewMi.Models.Content(data.content);
-        var movie = new ReviewMi.Models.Movie(data.movie);
-
-        ReviewMi.contents.add(content);
-        ReviewMi.movies.add(movie);
-
-        console.log('move ' + imdbID + ' has been found, ' + content.get('title') + ' has been added to the movies collection');
-
-        var view = new ReviewMi.Views.movieTitleView({model: movie});
-        view.render();
-
-      });
+    if ( !movie ) {
+      //perform search on omdb for this movie
+      movie = this.fetchMovie(imdbID, render);
 
     //movie already exists in movies collection, just render the view
     } else {
       console.log('this movie already exists in movies collection, no search needed');
-      //render the single movie view
-      var view = new ReviewMi.Views.movieTitleView({model: movieArr[0]});
-      view.render();  
+      render(movie);
     }
 
   },
 
+  //create a new review page (for content)
   newReview: function (id) {
     //find the content model associated with this route
     var model = ReviewMi.contents.get(id);
@@ -91,7 +74,38 @@ ReviewMi.Routers.appRouter = Backbone.Router.extend({
     view.render();
   },
   
-  goHome: function () {
-    document.location.hash = '';
+  // goHome: function () {
+  //   document.location.hash = '';
+  // },
+
+  //-------------------------------
+  // HELPER FUNCTIONS (NOT ROUTES)
+  //-------------------------------
+
+  fetchMovie: function(imdbID) {
+    console.log('this is a new movie, searching omdb...');
+
+    var request = $.ajax({
+      type: 'GET',
+      dataType: 'json',
+      url: '/movies/search/' + imdbID
+      // data: { s: searchTerm }
+    //what to do on success
+    }).done(function (data) {
+
+      // create a new 'content' and 'movie' and add these to the respective collections
+      var content = new ReviewMi.Models.Content(data.content);
+      var movie = new ReviewMi.Models.Movie(data.movie);
+
+      ReviewMi.contents.add(content);
+      ReviewMi.movies.add(movie);
+
+      console.log('move ' + imdbID + ' has been found, ' + content.get('title') + ' has been added to the movies collection');
+
+      //if a callback function was passed in, run this function
+      if (typeof success === 'function') {
+        success(movie);
+      }
+    });
   }
 });
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,13 @@
 class ReviewsController < ApplicationController
 
   # THIS DOESN'T WORK
-  # before_action :user_signed_in?, :only => [:create]
+  before_action :signin_required, :only => [:create]
 
   def index
     #FIX: change this to all reviews of friends
-    @reviews = Review.all
+    reviews = Review.all
 
-    render :json => @reviews
+    render :json => reviews #, :include => {:user => {:only => :username}}
   end
 
   # def show
@@ -27,11 +27,10 @@ class ReviewsController < ApplicationController
     end
   end
 
-  # private
-  # def user_needed
-  #   unless current_user
-  #     render :json => {:success => false, :message => "ERROR: you must be signed in to create a review"}, 
-  #                     :status => 401
-  #   end
-  # end
+  private
+  def signin_required
+    unless current_user
+      render :json => {:success => false, :status => 401, :message => "ERROR: you must be signed in to create a review"}
+    end
+  end
 end
