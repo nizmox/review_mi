2c83fe3c35cef5ab77d1a2bf51b427800c6e457f
Nizmox <nizmox@gmail.com>
2014-05-21 11:58:22 +1000

    minor changes

--- spec/models/content_spec.rb
+++ spec/models/content_spec.rb
@@ -14,5 +14,39 @@
 require 'spec_helper'
 
 describe Content do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.new' do
+    let(:content) { Content.new }
+    subject { content }
+
+    it "should return a new content" do
+      subject.should_not be_nil
+    end
+
+    it { should respond_to(:title) }
+    it { should respond_to(:image) }
+  end
+
+  describe '.create' do
+    context 'with valid information' do
+      let(:content) do 
+        Content.create(:title => 'Jaws', :image => 'http://ia.media-imdb.com/images/M/MV5BNDcxODkyMjY4MF5BMl5BanBnXkFtZTgwOTk5NTc5MDE@._V1_SX300.jpg') 
+      end
+      subject { content }
+
+      its(:title) { should == 'Jaws'}
+      its(:image) { should == 'http://ia.media-imdb.com/images/M/MV5BNDcxODkyMjY4MF5BMl5BanBnXkFtZTgwOTk5NTc5MDE@._V1_SX300.jpg'}
+      
+      # should be valid
+      it "should be valid" do
+        subject.should be_valid
+      end
+    end
+  end
+
+  # describe 'with invalid data' do
+  #   context 'title cannot be blank' do
+  #     content = Content.new(:title => '')
+  #     content.valid?.should be_false
+  #   end
+  # end
 end
